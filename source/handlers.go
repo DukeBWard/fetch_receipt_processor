3c9065aff16aeb683af2588493e13a877b40cf72
Luke Ward <wardlukeb@gmail.com>
2024-10-30 10:26:51 -0400

    added post receipt handler

--- source/handlers.go
+++ source/handlers.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi"
+	"github.com/google/uuid"
 )
 
 // just keep it in memory for now
@@ -12,6 +13,25 @@ var receipts = make(map[string]Receipt)
 
 // POST /receipts/process
 func postReceiptHandler(w http.ResponseWriter, r *http.Request) {
+
+	var receipt Receipt
+	err := json.NewDecoder(r.Body).Decode(&receipt)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// generate a unique id for the receipt
+	id := uuid.New().String()
+	receipt.ID = id
+	receipts[id] = receipt
+
+	// TODO: calculate points
+	receipt.Points = calcPoints(receipt)
+
+	res := map[string]string{"id": id}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(res)
 
 }
 
